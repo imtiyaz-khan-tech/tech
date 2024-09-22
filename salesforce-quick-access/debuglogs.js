1a35389c1ea1340800ce1d2d5df2d97edc9cadf4
Imtiyaz Khan <imtiyaz.khan.techno@gmail.com>
2024-09-22 16:15:59 +0530

    c

--- salesforce-quick-access/debuglogs.js
+++ salesforce-quick-access/debuglogs.js
@@ -385,7 +385,7 @@ async function fetchRecordsXMLSOAP(query) {
         headers: {
             "Content-Type": "text/xml",
             "SOAPAction": "urn:enterprise.soap.sforce.com/query",
-            mode: 'no-cors'
+            'Access-Control-Allow-Origin': 'https://imtiyaz-khan-tech.github.io'
         },
         body: soapRequest,
     }).then(async (response) => await response.text()).then((recs) => {
