5ed032bd35c18ede1c63fba6d5d7139e9f683336
Imtiyaz Khan <imtiyaz.khan.techno@gmail.com>
2024-09-28 14:38:54 +0530

    Excel Libraries

--- excel-operator/excel-operator.js
+++ excel-operator/excel-operator.js
@@ -15,36 +15,22 @@ $(document).on('click', '.btn', function(e) {
         clearBottom();
     } else if (btn == 'Include Columns') {
         let columsArray = $('.txt_area').val().split('\n');
-        console.log('$columsArray: ',columsArray);
         includeColumns(columsArray);
     } else if (btn == 'Exclude Columns') {
         let columsArray = $('.txt_area').val().split('\n');
-        console.log('$columsArray: ',columsArray);
         excludeColumns(columsArray);
     } else if (btn == 'Format Date') {
         formateDateColumn($('.inp_formatted_date').val());
-    } else if (btn == 'Copy Excel - 1') {
-        let tableText = '';
-        $('.t_x_table tr').each(function() {
-            let rowText = '';
-            $(this).find('th, td').each(function() {
-                rowText += $(this).text() + '\t';
-            });
-            tableText += rowText.trim() + '\n';
-        });
-        copyToCLipboard(tableText.trim().split('\n').filter(Boolean).join('\n'));
-        /* const blob = new Blob([$('.cleftdvs_top').html()], { type: "text/html" });
-        navigator.clipboard.write([new ClipboardItem({ [blob.type]: blob })]); */
-    } else if (btn == 'Copy Excel - 2') {
-        let tableText = '';
-        $('.b_x_table tr').each(function() {
-            let rowText = '';
-            $(this).find('th, td').each(function() {
-                rowText += $(this).text() + '\t';
-            });
-            tableText += rowText.trim() + '\n';
-        });
-        copyToCLipboard(tableText.trim().split('\n').filter(Boolean).join('\n'));
+    }else if (btn == 'Copy Top') {
+        let html = $('.cleftdvs_top').html();
+        html = html.replace(/(<td\b[^>]*?)\s*style="[^"]*"/g, '$1');
+        const blob = new Blob([html], { type: "text/html" });
+        navigator.clipboard.write([new ClipboardItem({ [blob.type]: blob })]);
+    } else if (btn == 'Copy Bottom') {
+        let html = $('.cleftdvs_bottom').html();
+        html = html.replace(/(<td\b[^>]*?)\s*style="[^"]*"/g, '$1');
+        const blob = new Blob([html], { type: "text/html" });
+        navigator.clipboard.write([new ClipboardItem({ [blob.type]: blob })]);
     }
 });
 
@@ -68,49 +54,53 @@ function copyToCLipboard(value) {
 function pasteExcel1() {
     navigator.clipboard.readText().then(text => {
         excelData1 = text;
-        generateExcelTableTop();
+        convertExcelToJson_Top();
     }).catch(err => {
         console.error(err);
     });
 }
-
-function pasteExcel2() {
-    navigator.clipboard.readText().then(text => {
-        excelData2 = text;
-        generateExcelTableBottom();
-    }).catch(err => {
-        console.error(err);
+let excelJson_top = null;
+function convertExcelToJson_Top(){
+    const workbook = XLSX.read(excelData1, { type: 'string', raw: true });
+    const sheetName = workbook.SheetNames[0];
+    const worksheet = workbook.Sheets[sheetName];
+    const csvData = XLSX.utils.sheet_to_csv(worksheet);
+    Papa.parse(csvData, {
+        header: true,
+        dynamicTyping: true,
+        complete: function(results) {
+            console.log(results);
+            console.log(results.data);
+            excelJson_top = results.data;
+            generateJsonToTable_Top();
+        },
+        error: function(err) {
+            console.error(err);
+        }
     });
 }
 
-function generateExcelTableTop() {
-    let rows = excelData1.trim().split('\n');
-    let firstRowCells = rows[0].split('\t');
-
+function generateJsonToTable_Top(){
+    let columns = Object.keys(excelJson_top[0]);
+    console.log('$columns: ',columns);
     let i = 0;
     let ths = '';
-    // let f_inx = 0;
-
-    while (i < firstRowCells.length) {
-        ths += `<th class="t_x_th">${firstRowCells[i].trim()}</th>`;
-        /* if(firstRowCells[i].trim() == 'ALI_Soldto__c'){
-            f_inx = i;
-        } */
+    while (i < columns.length) {
+        let col = columns[i];
+        ths += `<td class="t_x_th">${getIdelValue(col)}</td>`;
         i++;
     }
+    console.log('$ths: ',ths);
 
-    i = 1;
+    i = 0;
     let trs = '';
-    while (i < rows.length) {
-        let cells = rows[i].split('\t');
-        let tds = '';
+    while(i < excelJson_top.length){
+        let item = excelJson_top[i];
         let j = 0;
-        while (j < cells.length) {
-            /* if(f_inx == j){
-                tds += `<td class="t_x_td" style="background-color:yellow;">${cells[j].trim()}</td>`;
-            }else{ */
-                tds += `<td class="t_x_td">${cells[j].trim()}</td>`;
-            //}
+        let tds = '';
+        while(j < columns.length){
+            let col = columns[j];
+            tds += `<td class="t_x_td">${getIdelValue(item[col])}</td>`;
             j++;
         }
         trs += `<tr  class="t_x_tr t_x_tb_tr">${tds}</tr>`;
@@ -130,126 +120,158 @@ function generateExcelTableTop() {
     `;
     $('.cleftdvs_top').html(table);
 }
-
-function generateExcelTableBottom() {
-    let rows = excelData2.trim().split('\n');
-    let firstRowCells = rows[0].split('\t');
-
-    let i = 0;
-    let ths = '';
-    while (i < firstRowCells.length) {
-        ths += `<th class="b_x_th">${firstRowCells[i].trim()}</th>`;
-        i++;
-    }
-
-    i = 1;
-    let trs = '';
-    while (i < rows.length) {
-        let cells = rows[i].split('\t');
-        let tds = '';
-        let j = 0;
-        while (j < cells.length) {
-            tds += `<td class="b_x_td">${cells[j].trim()}</td>`;
-            j++;
+let excelJson_bottom = null;
+function convertExcelToJson_Bottom(){
+    const workbook = XLSX.read(excelData2, { type: 'string', raw: true });
+    const sheetName = workbook.SheetNames[0];
+    const worksheet = workbook.Sheets[sheetName];
+    const csvData = XLSX.utils.sheet_to_csv(worksheet);
+    Papa.parse(csvData, {
+        header: true,
+        dynamicTyping: true,
+        complete: function(results) {
+            console.log(results);
+            console.log(results.data);
+            excelJson_bottom = results.data;
+            generateJsonToTable_Bottom();
+        },
+        error: function(err) {
+            console.error(err);
         }
-        trs += `<tr  class="b_x_tr b_x_tb_tr">${tds}</tr>`;
-        i++;
-    }
-    let table = `
-        <table class="b_x_table">
-            <thead class="b_x_thead">
-                <tr  class="b_x_tr b_x_th_tr">
-                    ${ths}
-                </tr>
-            </thead>
-            <tbody class="b_x_body">
-                ${trs}
-            </tbody>
-        </table>
-    `;
-    $('.cleftdvs_bottom').html(table);
+    });
 }
 
-function includeColumns(columsArray){
-    let rows = excelData1.trim().split('\n');
-    let firstRowCells = rows[0].split('\t');
-
+function generateJsonToTable_Bottom(){
+    let columns = Object.keys(excelJson_bottom[0]);
+    console.log('$columns: ',columns);
     let i = 0;
     let ths = '';
-    let f_inx = 0;
-
-    let indexes = [];
-
-    while (i < firstRowCells.length) {
-        if(columsArray.includes(firstRowCells[i].trim())){
-            ths += `<th class="b_x_th">${firstRowCells[i].trim()}</th>`;
-            indexes.push(i);
-        }
-        i++;
-    }
-
-    i = 1;
-    let trs = '';
-    while (i < rows.length) {
-        let cells = rows[i].split('\t');
-        let tds = '';
-        let j = 0;
-        while (j < cells.length) {
-            if(indexes.includes(j)){
-                tds += `<td class="b_x_td">${cells[j].trim()}</td>`;
-            }
-            j++;
-        }
-        trs += `<tr  class="b_x_tr b_x_tb_tr">${tds}</tr>`;
-        i++;
-    }
-    let table = `
-        <table class="b_x_table">
-            <thead class="b_x_thead">
-                <tr  class="b_x_tr b_x_th_tr">
-                    ${ths}
-                </tr>
-            </thead>
-            <tbody class="b_x_body">
-                ${trs}
-            </tbody>
-        </table>
-    `;
-    $('.cleftdvs_bottom').html(table);
-}
-function excludeColumns(columsArray){
-    let rows = excelData1.trim().split('\n');
-    let firstRowCells = rows[0].split('\t');
-
-    let i = 0;
-    let ths = '';
-
-    let indexes = [];
-
-    while (i < firstRowCells.length) {
-        let col = firstRowCells[i].trim();
-        if(!columsArray.includes(col)){
-            console.log('$true-col: ',col);
-            ths += `<th class="b_x_th">${col}</th>`;
-            indexes.push(i);
-        }else{
-            console.log('$false-col: ',col);
-        }
+    while (i < columns.length) {
+        let col = columns[i];
+        ths += `<td class="b_x_th">${getIdelValue(col)}</td>`;
         i++;
     }
     console.log('$ths: ',ths);
-    console.log('$indexes: ',indexes);
 
-    i = 1;
+    i = 0;
     let trs = '';
-    while (i < rows.length) {
-        let cells = rows[i].split('\t');
-        let tds = '';
+    while(i < excelJson_bottom.length){
+        let item = excelJson_bottom[i];
         let j = 0;
-        while (j < cells.length) {
-            if(indexes.includes(j)){
-                tds += `<td class="b_x_td">${cells[j].trim()}</td>`;
-            }
+        let tds = '';
+        while(j < columns.length){
+            let col = columns[j];
+            tds += `<td class="b_x_td">${getIdelValue(item[col])}</td>`;
+            j++;
+        }
+        trs += `<tr  class="b_x_tr b_x_tb_tr">${tds}</tr>`;
+        i++;
+    }
+    let table = `
+        <table class="b_x_table">
+            <thead class="b_x_thead">
+                <tr  class="b_x_tr b_x_th_tr">
+                    ${ths}
+                </tr>
+            </thead>
+            <tbody class="b_x_body">
+                ${trs}
+            </tbody>
+        </table>
+    `;
+    $('.cleftdvs_bottom').html(table);
+}
+
+function getIdelValue(val){
+    if(typeof val == 'string'){
+        return val.trim().replace(/\n/g, '</br>');
+    }else if(typeof val == 'boolean'){
+        return val;
+    }else if(val == 0 || val == '0'){
+        return val;
+    }else if(!val){
+        return '';
+    }else{
+        return val;
+    }
+}
+
+function pasteExcel2() {
+    navigator.clipboard.readText().then(text => {
+        excelData2 = text;
+        convertExcelToJson_Bottom();
+    }).catch(err => {
+        console.error(err);
+    });
+}
+
+function includeColumns(columsArray){
+    let columns = Object.keys(excelJson_top[0]);
+    console.log('$columns: ',columns);
+    let i = 0;
+    let ths = '';
+    while (i < columns.length) {
+        let col = columns[i];
+        if(columsArray.includes(col))
+            ths += `<td class="b_x_th">${getIdelValue(col)}</td>`;
+        i++;
+    }
+    console.log('$ths: ',ths);
+
+    i = 0;
+    let trs = '';
+    while(i < excelJson_top.length){
+        let item = excelJson_top[i];
+        let j = 0;
+        let tds = '';
+        while(j < columns.length){
+            let col = columns[j];
+            if(columsArray.includes(col))
+                tds += `<td class="b_x_td">${getIdelValue(item[col])}</td>`;
+            j++;
+        }
+        trs += `<tr  class="b_x_tr b_x_tb_tr">${tds}</tr>`;
+        i++;
+    }
+    let table = `
+        <table class="b_x_table">
+            <thead class="b_x_thead">
+                <tr  class="b_x_tr b_x_th_tr">
+                    ${ths}
+                </tr>
+            </thead>
+            <tbody class="b_x_body">
+                ${trs}
+            </tbody>
+        </table>
+    `;
+    $('.cleftdvs_bottom').html(table);
+}
+
+
+function excludeColumns(columsArray){
+    let columns = Object.keys(excelJson_top[0]);
+    console.log('$columns: ',columns);
+    let i = 0;
+    let ths = '';
+    while (i < columns.length) {
+        let col = columns[i];
+        if(!columsArray.includes(col))
+            ths += `<td class="b_x_th">${getIdelValue(col)}</td>`;
+        i++;
+    }
+    console.log('$ths: ',ths);
+
+    i = 0;
+    let trs = '';
+    while(i < excelJson_top.length){
+        let item = excelJson_top[i];
+        let j = 0;
+        let tds = '';
+        while(j < columns.length){
+            let col = columns[j];
+            if(!columsArray.includes(col))
+                tds += `<td class="b_x_td">${getIdelValue(item[col])}</td>`;
             j++;
         }
         trs += `<tr  class="b_x_tr b_x_tb_tr">${tds}</tr>`;
@@ -290,62 +312,59 @@ $(document).on('click', '.t_x_th, .b_x_th', function (e){
 $(document).on('dblclick', '.t_x_th', function (e){
     $('.txt_area').val('');
  });
-
- function formateDateColumn(colum){
-    let rows = excelData1.trim().split('\n');
-    let firstRowCells = rows[0].split('\t').map(v => v.trim());
+function formateDateColumn(colum){
+    let columns = Object.keys(excelJson_top[0]);
+    console.log('$columns: ',columns);
     let newColumn = 'Formatted-Date';
-    let index = firstRowCells.indexOf(colum);
-    if (index != -1) {
-        firstRowCells.splice(index, 0, newColumn);
-    }
-
-    let i = 0;
-    let ths = '';
-    let newColumIndex = 0;
-    while (i < firstRowCells.length) {
-        let cell = firstRowCells[i].trim();
-        if(cell == newColumn){
-            newColumIndex = i;
+    let index = columns.indexOf(colum);
+    console.log('$index: ',index);
+    if(index != -1){
+        columns.splice(index, 0, newColumn);
+        let i = 0;
+        let ths = '';
+        while (i < columns.length) {
+            let col = columns[i];
+            ths += `<td class="b_x_th">${getIdelValue(col)}</td>`;
+            i++;
         }
-        ths += `<th class="b_x_th">${cell}</th>`;
-        i++;
-    }
+        console.log('$ths: ',ths);
 
-    i = 1;
-    let trs = '';
-    while (i < rows.length) {
-        let cells = rows[i].split('\t');
-        let tds = '';
-        let j = 0;
-        while (j < cells.length) {
-            if(j == newColumIndex){
-                let cell = cells[j].trim();
-                if(colum == 'StartDate'){
-                    tds += `<td class="b_x_td">${replaceBeforeChar(cell, '-')}</td>`;
+        i = 0;
+        let trs = '';
+        while(i < excelJson_top.length){
+            let item = excelJson_top[i];
+            let j = 0;
+            let tds = '';
+            while(j < columns.length){
+                let col = columns[j];
+                if(col == newColumn){
+                    if(colum == 'StartDate'){
+                        tds += `<td class="b_x_td">${replaceBeforeChar(item[colum], '-')}</td>`;
+                    }else{
+                        tds += `<td class="b_x_td">${replaceTextBetween(item[colum], '-', '-', '-')}</td>`;
+                    }
                 }else{
-                    tds += `<td class="b_x_td">${replaceTextBetween(cell, '-', '-', '-')}</td>`;
+                    tds += `<td class="b_x_td">${getIdelValue(item[col])}</td>`;
                 }
+                j++;
             }
-            tds += `<td class="b_x_td">${cells[j].trim()}</td>`;
-            j++;
+            trs += `<tr  class="b_x_tr b_x_tb_tr">${tds}</tr>`;
+            i++;
         }
-        trs += `<tr  class="b_x_tr b_x_tb_tr">${tds}</tr>`;
-        i++;
+        let table = `
+            <table class="b_x_table">
+                <thead class="b_x_thead">
+                    <tr  class="b_x_tr b_x_th_tr">
+                        ${ths}
+                    </tr>
+                </thead>
+                <tbody class="b_x_body">
+                    ${trs}
+                </tbody>
+            </table>
+        `;
+        $('.cleftdvs_bottom').html(table);
     }
-    let table = `
-        <table class="b_x_table">
-            <thead class="b_x_thead">
-                <tr  class="b_x_tr b_x_th_tr">
-                    ${ths}
-                </tr>
-            </thead>
-            <tbody class="b_x_body">
-                ${trs}
-            </tbody>
-        </table>
-    `;
-    $('.cleftdvs_bottom').html(table);
 }
 function replaceTextBetween(val, startChar, endChar, replaceValue){
     let regex = new RegExp(`\\${startChar}[^\\${endChar}]+\\${endChar}`, 'g');
