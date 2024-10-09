b185df114ffe2907e7b1769d12708809f12eb1c9
Imtiyaz Khan <imtiyaz.khan.techno@gmail.com>
2024-10-09 19:12:33 +0530

    c

--- text-tools/text-tool.js
+++ text-tools/text-tool.js
@@ -1,26 +1,28 @@
 $(document).ready(function() {
     // changeFavicon('Text Tools');
+
+    populateFromLocalStorage();
+
     Draggable.create('.small_popup_1', {
         handle: '.small_popup_1_box_heading_div'
     });
 
-    /* $('.txt_1').val(`10-AZ_2020
-01-AZ_2021
-04-AZ_2021
-07-AZ_2021
-10-AZ_2021
-01-AZ_2022
-04-AZ_2022
-07-AZ_2022
-10-AZ_2022
-01-AZ_2023
-04-AZ_2023
-07-AZ_2023
-10-AZ_2023
-01-AZ_2024
-04-AZ_2024
-07-AZ_2024`);
-}); */
+    /* $('.txt_1').val(`10-AZ-2020
+01-AZ-2021
+04-AZ-2021
+07-AZ-2021
+10-AZ-2021
+01-AZ-2022
+04-AZ-2022
+07-AZ-2022
+10-AZ-2022
+01-AZ-2023
+04-AZ-2023
+07-AZ-2023
+10-AZ-2023
+01-AZ-2024
+04-AZ-2024
+07-AZ-2024`); */
 });
 $(document).on('click', '.close-icon_1', function (e){
     // $('.d_outer').removeClass('blur');
@@ -101,11 +103,12 @@ $(document).on('click', '.btn', function(e) {
                 if(replace_value == '\\n'){
                     txt_2(txt1.replaceAll(/\n/g, replace_with).trim());
                 }else{
-                    txt_2(txt1.replaceAll(replace_value, replace_with).trim());
+                    txt_2(txt1.replaceAll(replace_value, replace_with));
+                    // txt_2(txt1.replaceAll(replace_value, replace_with).trim());
                 }
             }
         }
-
+        afterActions();
     }else if (btn == 'Add Left') {
         let left_right_value = inp('left_right_value');
         let txt1 = txt_1();
@@ -113,6 +116,7 @@ $(document).on('click', '.btn', function(e) {
             return left_right_value + l;
         });
         txt_2(txt1.join('\n'));
+        afterActions();
     }else if (btn == 'Add Right') {
         let left_right_value = inp('left_right_value');
         let txt1 = txt_1();
@@ -120,6 +124,7 @@ $(document).on('click', '.btn', function(e) {
             return l + left_right_value;
         });
         txt_2(txt1.join('\n'));
+        afterActions();
     }else if (btn == 'Add Left-Right') {
         let left_right_value = inp('left_right_value');
         let txt1 = txt_1();
@@ -127,6 +132,7 @@ $(document).on('click', '.btn', function(e) {
             return  left_right_value + l + left_right_value;
         });
         txt_2(txt1.join('\n'));
+        afterActions();
     }else if (btn == 'Trim') {
         let txt1 = txt_1();
         txt1 = txt1.trim().split('\n').map(l => l.trim());
@@ -134,7 +140,7 @@ $(document).on('click', '.btn', function(e) {
         txt_1(txt1.filter(Boolean).join('\n'));
     }else if (btn == 'Update') {
         if(txt_2())
-            txt_1(txt_2());
+            updateToTop();
     }else if (btn == 'Clear') {
         clear();
     }else if (btn == 'Split Excel') {
@@ -143,16 +149,18 @@ $(document).on('click', '.btn', function(e) {
         if(split_excel_value){
             txt1 = txt1.trim().split('\t').map(l => l.trim());
             txt_2(txt1.filter(Boolean).join(split_excel_value));
-            txt_1(txt_2());
+            updateToTop();
         }else{
             txt1 = txt1.trim().split('\t').map(l => l.trim());
             txt_2(txt1.filter(Boolean).join('\n'));
-            txt_1(txt_2());
+            updateToTop();
         }
+        afterActions();
     }else if (btn == 'In Query') {
         let txt1 = txt_1();
         txt1 = txt1.trim().split('\n').map(l => l.trim());
         txt_2( `'` + txt1.filter(Boolean).join(`','`) + `'`);
+        afterActions();
     }else if (btn == 'In Query Skip Rows') {
         let txt1 = txt_1();
         let skip_row_count = inp('skip_row_count');
@@ -164,6 +172,7 @@ $(document).on('click', '.btn', function(e) {
             txt1.splice(0, skip_row_count);
             txt_2( `'` + txt1.filter(Boolean).join(`','`) + `'`);
         }
+        afterActions();
     }else if (btn == 'Copy') {
         copyToCLipboard(txt_2(), $(this));
     }else if (btn == 'Blank') {
@@ -179,16 +188,19 @@ $(document).on('click', '.btn', function(e) {
             i++;
         }
         txt_2(noDuplicatesArray.filter(Boolean).join(`\n`));
+        afterActions();
     }else if (btn == 'Generate Array') {
         let txt1 = txt_1();
         txt1 = txt1.trim().split('\n').map(l => l.trim());
         txt_2(`['${txt1.join(`', '`)}']`);
+        afterActions();
     }else if (btn == 'Generate Dropdown') {
         let txt1 = txt_1();
         txt1 = txt1.trim().split('\n').map(l => {
             return `{ label: "${l.trim()}", value: "${l.trim()}" }`;
         });
         txt_2(`[\n\t${txt1.join(`,\n\t`)}\n];`);
+        afterActions();
     }else if (btn == 'Extract Text Between') {
         let txt1 = txt_1();
         let extract_left_value = inp('extract_left_value');
@@ -201,6 +213,7 @@ $(document).on('click', '.btn', function(e) {
             const outputArray = matches.map(match => match.replace(startTag, '').replace(endTag, ''));
             txt_2(outputArray.join('\n'));
         }
+        afterActions();
     }else if (btn == 'Replace Text Between - Include') {
         let txt1 = txt_1();
         let replace_left_value = inp('replace_left_value');
@@ -242,6 +255,7 @@ $(document).on('click', '.btn', function(e) {
             txt_2(noDuplicatesArray.filter(Boolean).join(`\n`));
             */
         }
+        afterActions();
     }else if (btn == 'Configure User Snippet') {
         let txt1 = txt_1();
         let txt2 = txt_2();
@@ -276,9 +290,11 @@ $(document).on('click', '.btn', function(e) {
         txt2 = txt2.replaceAll(b, '"');
         txt2 = txt2.substring(0, txt2.length - 1);
         txt_2(txt2);
+        afterActions();
     }else if (btn == 'Extract Text From Html') {
         let txt1 = txt_1();
         txt_2(htmlDecode(txt1));
+        afterActions();
     }else if (btn == 'In Query Distinct') {
         navigator.clipboard.readText().then(text => {
             let i = 0;
@@ -300,6 +316,7 @@ $(document).on('click', '.btn', function(e) {
         }).catch(err => {
            console.error(err);
         });
+        afterActions();
     }else if (btn == 'Test Data 1') {
         let testDataArray = [];
         let i = 1;
@@ -315,6 +332,7 @@ $(document).on('click', '.btn', function(e) {
             i++;
         }
         txt_1(testDataArray.join('\n'));
+        afterActions();
     }else if (btn == 'Test Data 2') {
         let testDataArray = [];
         let i = 1;
@@ -324,10 +342,13 @@ $(document).on('click', '.btn', function(e) {
             i++;
         }
         txt_1(testDataArray.join('\n'));
+        afterActions();
     }else if (btn == 'Test Data 3') {
         txt_1('Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam necessitatibus tempore cum quos molestias repellat ducimus maiores quam nobis, quia perferendis illum numquam sint veritatis alias eveniet non quaerat vel!');
+        afterActions();
     }else if (btn == 'Test Data 4') {
         txt_1('Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam necessitatibus tempore cum quos molestias repellat ducimus maiores quam nobis, quia perferendis illum numquam sint veritatis alias eveniet non quaerat vel!');
+        afterActions();
     }else if (btn == 'Replace Before') {
         let txt1 = txt_1();
         let replace_before_value = inp('replace_before_value');
@@ -335,6 +356,7 @@ $(document).on('click', '.btn', function(e) {
         if(replace_before_value){
             txt_2(txt1.trim().split('\n').map(l => replaceBeforeChar(l.trim(), replace_before_value, replace_before_with_value)).join('\n'));
         }
+        afterActions();
     }else if (btn == 'Replace After') {
         let txt1 = txt_1();
         let replace_after_value = inp('replace_after_value');
@@ -342,6 +364,7 @@ $(document).on('click', '.btn', function(e) {
         if(replace_after_value){
             txt_2(txt1.trim().split('\n').map(l => replaceAfterChar(l.trim(), replace_after_value, replace_after_with_value)).join('\n'));
         }
+        afterActions();
     }
 });
 
@@ -453,4 +476,86 @@ function replaceAfterChar(val, startChar, replaceValue) {
     let regex = new RegExp(`${startChar}.*`, 'g');
     let result = val.replace(regex, replaceValue);
     return result;
+}
+$(document).on('input', '.inp', function (e){
+    doOnInput($(this));
+});
+
+function populateFromLocalStorage(){
+    if(local('replace_value')){
+        $('.replace_value').val(local('replace_value'));
+    }
+    if(local('replace_with')){
+        $('.replace_with').val(local('replace_with'));
+    }
+    if(local('split_excel_value')){
+        $('.split_excel_value').val(local('split_excel_value'));
+    }
+    if(local('extract_left_value')){
+        $('.extract_left_value').val(local('extract_left_value'));
+    }
+    if(local('extract_right_value')){
+        $('.extract_right_value').val(local('extract_right_value'));
+    }
+    if(local('replace_left_value')){
+        $('.replace_left_value').val(local('replace_left_value'));
+    }
+    if(local('replace_right_value')){
+        $('.replace_right_value').val(local('replace_right_value'));
+    }
+    if(local('replace_value_btwn')){
+        $('.replace_value_btwn').val(local('replace_value_btwn'));
+    }
+    if(local('replace_before_value')){
+        $('.replace_before_value').val(local('replace_before_value'));
+    }
+    if(local('replace_before_with_value')){
+        $('.replace_before_with_value').val(local('replace_before_with_value'));
+    }
+    if(local('replace_after_value')){
+        $('.replace_after_value').val(local('replace_after_value'));
+    }
+    if(local('replace_after_with_value')){
+        $('.replace_after_with_value').val(local('replace_after_with_value'));
+    }
+    if(local('left_right_value')){
+        $('.left_right_value').val(local('left_right_value'));
+    }
+}
+
+function local(key){
+    return localStorage.getItem(key);
+}
+
+function doOnInput(_this){
+    let val = _this.val().trim();
+   if(val){
+       localStorage.setItem(_this.data('cls'),val);
+   }else{
+       localStorage.removeItem(_this.data('cls'));
+   }
+}
+
+function updateToTop(){
+    txt_1(txt_2());
+}
+
+let isCtrlPressed = false;
+$(document).on('keydown', 'body', function (e){
+   let charCode = (e.which) ? e.which : e.keyCode;
+   if(charCode == 17){
+      isCtrlPressed = true;
+      console.log(isCtrlPressed);
+   }
+});
+$(document).on('keyup', 'body', function (e){
+   let charCode = (e.which) ? e.which : e.keyCode;
+   isCtrlPressed = charCode == 17 ? false : isCtrlPressed;
+   console.log(isCtrlPressed);
+});
+
+function afterActions(){
+    if(isCtrlPressed){
+        updateToTop();
+    }
 }
