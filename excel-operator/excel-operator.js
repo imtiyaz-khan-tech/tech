3186aa5661835b6400324521b9e26339efb52327
Imtiyaz Khan <imtiyaz.khan.techno@gmail.com>
2024-10-15 19:06:13 +0530

    c

--- excel-operator/excel-operator.js
+++ excel-operator/excel-operator.js
@@ -140,6 +140,7 @@ function fillMembers(columsArray){
                 }else if(col == 'CreatedById'){
                     tds += `<td class="b_x_td">0054x000007ae7TAAQ</td>`;
                 }else if(col == 'Name'){
+                    //Select Id, p66_Rebate_Program__r.Name From Product2 Where p66_Rebate_Program__c != null
                     tds += `<td class="b_x_td">Member-${productIdAndRebateProgramNameMap.get(item['p66_Product__c'])}</td>`;
                 }
                 j++;
