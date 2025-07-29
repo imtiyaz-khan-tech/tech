cd41a8be200d88674ac09ff76d55b33c3ab26c00
Imtiyaz Khan <imtiyaz.khan.techno@gmail.com>
2025-07-29 13:41:38 +0530

    table-operator

---
+++ table-operator/table-operator.js
@@ -0,0 +1,487 @@
+let recs = [
+  {
+    "attributes": {
+      "type": "Account",
+      "url": "/services/data/v59.0/sobjects/Account/001dL000005gObpQAE"
+    },
+    "Id": "001dL000005gObpQAE",
+    "Name": "Sample Account for Entitlements",
+    "CreatedBy": {
+      "attributes": {
+        "type": "User",
+        "url": "/services/data/v59.0/sobjects/User/005dL000001T86UQAS"
+      },
+      "Name": "Automated Process"
+    },
+    "CreatedDate": "2024-05-22T12:02:13.000+0000",
+    "LastModifiedDate": "2024-05-22T12:02:13.000+0000",
+    "LastModifiedBy": {
+      "attributes": {
+        "type": "User",
+        "url": "/services/data/v59.0/sobjects/User/005dL000001T86UQAS"
+      },
+      "Name": "Automated Process"
+    },
+    "Owner": {
+      "attributes": {
+        "type": "User",
+        "url": "/services/data/v59.0/sobjects/User/005dL000001T86UQAS"
+      },
+      "Name": "Automated Process"
+    }
+  },
+  {
+    "attributes": {
+      "type": "Account",
+      "url": "/services/data/v59.0/sobjects/Account/001dL000005hBv1QAE"
+    },
+    "Id": "001dL000005hBv1QAE",
+    "Name": "Edge Communications",
+    "CreatedBy": {
+      "attributes": {
+        "type": "User",
+        "url": "/services/data/v59.0/sobjects/User/005dL000001T84rQAC"
+      },
+      "Name": "Imtiyaj Khan"
+    },
+    "CreatedDate": "2024-05-22T11:59:40.000+0000",
+    "LastModifiedDate": "2024-05-22T11:59:40.000+0000",
+    "LastModifiedBy": {
+      "attributes": {
+        "type": "User",
+        "url": "/services/data/v59.0/sobjects/User/005dL000001T84rQAC"
+      },
+      "Name": "Imtiyaj Khan"
+    },
+    "Owner": {
+      "attributes": {
+        "type": "User",
+        "url": "/services/data/v59.0/sobjects/User/005dL000001T84rQAC"
+      },
+      "Name": "Imtiyaj Khan"
+    }
+  },
+  {
+    "attributes": {
+      "type": "Account",
+      "url": "/services/data/v59.0/sobjects/Account/001dL000005hBv2QAE"
+    },
+    "Id": "001dL000005hBv2QAE",
+    "Name": "Burlington Textiles Corp of America",
+    "CreatedBy": {
+      "attributes": {
+        "type": "User",
+        "url": "/services/data/v59.0/sobjects/User/005dL000001T84rQAC"
+      },
+      "Name": "Imtiyaj Khan"
+    },
+    "CreatedDate": "2024-05-22T11:59:40.000+0000",
+    "LastModifiedDate": "2024-05-22T11:59:40.000+0000",
+    "LastModifiedBy": {
+      "attributes": {
+        "type": "User",
+        "url": "/services/data/v59.0/sobjects/User/005dL000001T84rQAC"
+      },
+      "Name": "Imtiyaj Khan"
+    },
+    "Owner": {
+      "attributes": {
+        "type": "User",
+        "url": "/services/data/v59.0/sobjects/User/005dL000001T84rQAC"
+      },
+      "Name": "Imtiyaj Khan"
+    }
+  },
+  {
+    "attributes": {
+      "type": "Account",
+      "url": "/services/data/v59.0/sobjects/Account/001dL000005hBv3QAE"
+    },
+    "Id": "001dL000005hBv3QAE",
+    "Name": "Pyramid Construction Inc.",
+    "CreatedBy": {
+      "attributes": {
+        "type": "User",
+        "url": "/services/data/v59.0/sobjects/User/005dL000001T84rQAC"
+      },
+      "Name": "Imtiyaj Khan"
+    },
+    "CreatedDate": "2024-05-22T11:59:40.000+0000",
+    "LastModifiedDate": "2024-05-22T11:59:40.000+0000",
+    "LastModifiedBy": {
+      "attributes": {
+        "type": "User",
+        "url": "/services/data/v59.0/sobjects/User/005dL000001T84rQAC"
+      },
+      "Name": "Imtiyaj Khan"
+    },
+    "Owner": {
+      "attributes": {
+        "type": "User",
+        "url": "/services/data/v59.0/sobjects/User/005dL000001T84rQAC"
+      },
+      "Name": "Imtiyaj Khan"
+    }
+  },
+  {
+    "attributes": {
+      "type": "Account",
+      "url": "/services/data/v59.0/sobjects/Account/001dL000005hBv4QAE"
+    },
+    "Id": "001dL000005hBv4QAE",
+    "Name": "Dickenson plc",
+    "CreatedBy": {
+      "attributes": {
+        "type": "User",
+        "url": "/services/data/v59.0/sobjects/User/005dL000001T84rQAC"
+      },
+      "Name": "Imtiyaj Khan"
+    },
+    "CreatedDate": "2024-05-22T11:59:40.000+0000",
+    "LastModifiedDate": "2024-05-22T11:59:40.000+0000",
+    "LastModifiedBy": {
+      "attributes": {
+        "type": "User",
+        "url": "/services/data/v59.0/sobjects/User/005dL000001T84rQAC"
+      },
+      "Name": "Imtiyaj Khan"
+    },
+    "Owner": {
+      "attributes": {
+        "type": "User",
+        "url": "/services/data/v59.0/sobjects/User/005dL000001T84rQAC"
+      },
+      "Name": "Imtiyaj Khan"
+    }
+  },
+  {
+    "attributes": {
+      "type": "Account",
+      "url": "/services/data/v59.0/sobjects/Account/001dL000005hBv5QAE"
+    },
+    "Id": "001dL000005hBv5QAE",
+    "Name": "Grand Hotels & Resorts Ltd",
+    "CreatedBy": {
+      "attributes": {
+        "type": "User",
+        "url": "/services/data/v59.0/sobjects/User/005dL000001T84rQAC"
+      },
+      "Name": "Imtiyaj Khan"
+    },
+    "CreatedDate": "2024-05-22T11:59:40.000+0000",
+    "LastModifiedDate": "2024-05-22T11:59:40.000+0000",
+    "LastModifiedBy": {
+      "attributes": {
+        "type": "User",
+        "url": "/services/data/v59.0/sobjects/User/005dL000001T84rQAC"
+      },
+      "Name": "Imtiyaj Khan"
+    },
+    "Owner": {
+      "attributes": {
+        "type": "User",
+        "url": "/services/data/v59.0/sobjects/User/005dL000001T84rQAC"
+      },
+      "Name": "Imtiyaj Khan"
+    }
+  },
+  {
+    "attributes": {
+      "type": "Account",
+      "url": "/services/data/v59.0/sobjects/Account/001dL000005hBv6QAE"
+    },
+    "Id": "001dL000005hBv6QAE",
+    "Name": "United Oil & Gas Corp.",
+    "CreatedBy": {
+      "attributes": {
+        "type": "User",
+        "url": "/services/data/v59.0/sobjects/User/005dL000001T84rQAC"
+      },
+      "Name": "Imtiyaj Khan"
+    },
+    "CreatedDate": "2024-05-22T11:59:40.000+0000",
+    "LastModifiedDate": "2024-05-22T11:59:40.000+0000",
+    "LastModifiedBy": {
+      "attributes": {
+        "type": "User",
+        "url": "/services/data/v59.0/sobjects/User/005dL000001T84rQAC"
+      },
+      "Name": "Imtiyaj Khan"
+    },
+    "Owner": {
+      "attributes": {
+        "type": "User",
+        "url": "/services/data/v59.0/sobjects/User/005dL000001T84rQAC"
+      },
+      "Name": "Imtiyaj Khan"
+    }
+  },
+  {
+    "attributes": {
+      "type": "Account",
+      "url": "/services/data/v59.0/sobjects/Account/001dL000005hBv7QAE"
+    },
+    "Id": "001dL000005hBv7QAE",
+    "Name": "Express Logistics and Transport",
+    "CreatedBy": {
+      "attributes": {
+        "type": "User",
+        "url": "/services/data/v59.0/sobjects/User/005dL000001T84rQAC"
+      },
+      "Name": "Imtiyaj Khan"
+    },
+    "CreatedDate": "2024-05-22T11:59:40.000+0000",
+    "LastModifiedDate": "2024-05-22T11:59:40.000+0000",
+    "LastModifiedBy": {
+      "attributes": {
+        "type": "User",
+        "url": "/services/data/v59.0/sobjects/User/005dL000001T84rQAC"
+      },
+      "Name": "Imtiyaj Khan"
+    },
+    "Owner": {
+      "attributes": {
+        "type": "User",
+        "url": "/services/data/v59.0/sobjects/User/005dL000001T84rQAC"
+      },
+      "Name": "Imtiyaj Khan"
+    }
+  },
+  {
+    "attributes": {
+      "type": "Account",
+      "url": "/services/data/v59.0/sobjects/Account/001dL000005hBv8QAE"
+    },
+    "Id": "001dL000005hBv8QAE",
+    "Name": "University of Arizona",
+    "CreatedBy": {
+      "attributes": {
+        "type": "User",
+        "url": "/services/data/v59.0/sobjects/User/005dL000001T84rQAC"
+      },
+      "Name": "Imtiyaj Khan"
+    },
+    "CreatedDate": "2024-05-22T11:59:40.000+0000",
+    "LastModifiedDate": "2024-05-22T11:59:40.000+0000",
+    "LastModifiedBy": {
+      "attributes": {
+        "type": "User",
+        "url": "/services/data/v59.0/sobjects/User/005dL000001T84rQAC"
+      },
+      "Name": "Imtiyaj Khan"
+    },
+    "Owner": {
+      "attributes": {
+        "type": "User",
+        "url": "/services/data/v59.0/sobjects/User/005dL000001T84rQAC"
+      },
+      "Name": "Imtiyaj Khan"
+    }
+  },
+  {
+    "attributes": {
+      "type": "Account",
+      "url": "/services/data/v59.0/sobjects/Account/001dL000005hBv9QAE"
+    },
+    "Id": "001dL000005hBv9QAE",
+    "Name": "United Oil & Gas, UK",
+    "CreatedBy": {
+      "attributes": {
+        "type": "User",
+        "url": "/services/data/v59.0/sobjects/User/005dL000001T84rQAC"
+      },
+      "Name": "Imtiyaj Khan"
+    },
+    "CreatedDate": "2024-05-22T11:59:40.000+0000",
+    "LastModifiedDate": "2024-05-22T11:59:40.000+0000",
+    "LastModifiedBy": {
+      "attributes": {
+        "type": "User",
+        "url": "/services/data/v59.0/sobjects/User/005dL000001T84rQAC"
+      },
+      "Name": "Imtiyaj Khan"
+    },
+    "Owner": {
+      "attributes": {
+        "type": "User",
+        "url": "/services/data/v59.0/sobjects/User/005dL000001T84rQAC"
+      },
+      "Name": "Imtiyaj Khan"
+    }
+  },
+  {
+    "attributes": {
+      "type": "Account",
+      "url": "/services/data/v59.0/sobjects/Account/001dL000005hBvAQAU"
+    },
+    "Id": "001dL000005hBvAQAU",
+    "Name": "United Oil & Gas, Singapore",
+    "CreatedBy": {
+      "attributes": {
+        "type": "User",
+        "url": "/services/data/v59.0/sobjects/User/005dL000001T84rQAC"
+      },
+      "Name": "Imtiyaj Khan"
+    },
+    "CreatedDate": "2024-05-22T11:59:40.000+0000",
+    "LastModifiedDate": "2024-05-22T11:59:40.000+0000",
+    "LastModifiedBy": {
+      "attributes": {
+        "type": "User",
+        "url": "/services/data/v59.0/sobjects/User/005dL000001T84rQAC"
+      },
+      "Name": "Imtiyaj Khan"
+    },
+    "Owner": {
+      "attributes": {
+        "type": "User",
+        "url": "/services/data/v59.0/sobjects/User/005dL000001T84rQAC"
+      },
+      "Name": "Imtiyaj Khan"
+    }
+  },
+  {
+    "attributes": {
+      "type": "Account",
+      "url": "/services/data/v59.0/sobjects/Account/001dL000005hBvBQAU"
+    },
+    "Id": "001dL000005hBvBQAU",
+    "Name": "GenePoint",
+    "CreatedBy": {
+      "attributes": {
+        "type": "User",
+        "url": "/services/data/v59.0/sobjects/User/005dL000001T84rQAC"
+      },
+      "Name": "Imtiyaj Khan"
+    },
+    "CreatedDate": "2024-05-22T11:59:40.000+0000",
+    "LastModifiedDate": "2024-05-22T11:59:40.000+0000",
+    "LastModifiedBy": {
+      "attributes": {
+        "type": "User",
+        "url": "/services/data/v59.0/sobjects/User/005dL000001T84rQAC"
+      },
+      "Name": "Imtiyaj Khan"
+    },
+    "Owner": {
+      "attributes": {
+        "type": "User",
+        "url": "/services/data/v59.0/sobjects/User/005dL000001T84rQAC"
+      },
+      "Name": "Imtiyaj Khan"
+    }
+  },
+  {
+    "attributes": {
+      "type": "Account",
+      "url": "/services/data/v59.0/sobjects/Account/001dL000005hBvCQAU"
+    },
+    "Id": "001dL000005hBvCQAU",
+    "Name": "sForce",
+    "CreatedBy": {
+      "attributes": {
+        "type": "User",
+        "url": "/services/data/v59.0/sobjects/User/005dL000001T84rQAC"
+      },
+      "Name": "Imtiyaj Khan"
+    },
+    "CreatedDate": "2024-05-22T11:59:40.000+0000",
+    "LastModifiedDate": "2024-05-22T11:59:40.000+0000",
+    "LastModifiedBy": {
+      "attributes": {
+        "type": "User",
+        "url": "/services/data/v59.0/sobjects/User/005dL000001T84rQAC"
+      },
+      "Name": "Imtiyaj Khan"
+    },
+    "Owner": {
+      "attributes": {
+        "type": "User",
+        "url": "/services/data/v59.0/sobjects/User/005dL000001T84rQAC"
+      },
+      "Name": "Imtiyaj Khan"
+    }
+  }
+];
+$(document).ready(function () {
+    creaetTable();
+});
+
+$(document).on('click', '.btn-fixed', function (e) {
+    navigator.clipboard.readText().then(recordsJson => {
+       console.log('$recordsJson ', recordsJson);
+       recs = JSON.parse(recordsJson);
+       creaetTable();
+    }).catch(err => {
+       console.error('Failed to read clipboard contents: ', err);
+    });
+});
+
+function copyTable() {
+    let html = $('.tableContainer').html();
+    console.log(html);
+    navigator.clipboard.writeText(html).then(function () {
+        console.log('copied : ' + html);
+    }, function (err) {
+        alert('Failed Copying.');
+        console.error('error copying');
+    });
+}
+
+function creaetTable() {
+
+    let i = 0;
+    while (i < recs.length) {
+        let item = recs[i];
+        if (item?.CreatedBy?.Name)
+            item.CreatedBy = item?.CreatedBy?.Name;
+        if (item?.LastModifiedBy?.Name)
+            item.LastModifiedBy = item?.LastModifiedBy?.Name;
+        if (item?.Owner?.Name)
+            item.Owner = item?.Owner?.Name;
+        if (item?.attributes)
+            delete item.attributes;
+        i++;
+    }
+    console.log(recs);
+
+    $('.tableContainer').html(createTableFromObjects(recs));
+}
+
+function createTableFromObjects(data) {
+    if (!data.length) return "<p>No data available</p>";
+
+    const columns = Object.keys(data[0]);
+    console.log('$columns: ', columns);
+
+    let table = "<table border='1' cellspacing='0' cellpadding='5'><thead><tr>";
+    columns.forEach(col => {
+        table += `<th>${col}</th>`;
+    });
+    table += "</tr></thead><tbody>";
+
+    data.forEach(obj => {
+        table += "<tr>";
+        columns.forEach(col => {
+            let val = obj[col];
+            if (col == 'CreatedDate' || col == 'LastModifiedDate') {
+                val = getFormattedDateTime(val);
+            }
+            let clss = '';
+            if (col == 'p66_Error_Message__c') {
+                clss = 'class="wrap-colum"';
+            }
+            table += `<td ${clss}>${val ?? ""}</td>`;
+        });
+        table += "</tr>";
+    });
+
+    table += "</tbody></table>";
+    document.title = 'Table [ ' + data.length + ']';
+    return table;
+}
+function getFormattedDateTime(dateString) {
+    let date_time = Intl.DateTimeFormat('en-IN', { timeZone: 'Asia/Kolkata', year: '2-digit', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit' }).format(new Date(dateString));
+    date_time = date_time.replaceAll(',', '').toUpperCase().split(' ').join(' ');
+    return date_time;
+}
